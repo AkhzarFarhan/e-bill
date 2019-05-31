9297438d74f9e3f7d7294876de740842eed2ae2f
Prakash Kumar <39598067+prakashkm03@users.noreply.github.com>
2019-05-31 11:27:04 +0530

    js for graph

---
+++ main.js
@@ -0,0 +1,15 @@
+var Data = {
+  'FRUITS': 1800,
+  'GROCERIES': 3600
+};
+
+var Options = {
+  'height': 200,
+  // 'title': '',
+  'width': 10,
+  'fixPadding': 18,
+  'barFont': [0, 10, "thin"],
+  'labelFont': [0, 8, 0]
+};
+
+graphite(Data, Options, example);
